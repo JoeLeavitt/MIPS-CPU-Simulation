44ae65b05f8b8c7cb775a87aac36a1d9c503a6a3
Joe Leavitt <joeleav1@gmail.com>
2015-08-07 15:31:37 -0400

    Fix bug regarding int and char inconsistency

--- project.c
+++ project.c
@@ -5,56 +5,61 @@
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
-	if(ALUControl == '0')
+	if((int)ALUControl == 000)
 	{
 		*ALUresult = A + B;
 	}
-	else if(ALUControl == '1')
+	else if((int)ALUControl == 001)
 	{
 		*ALUresult = A - B;
 	}
-	else if(ALUControl == '2')
-	{
-		if(A < B)
-			*ALUresult = '1';
-		else
-			*ALUresult = '0';
-	}
-	else if(ALUControl == '3')
+	else if((int)ALUControl == 010)
 	{
 		if((int)A < (int)B)
-			*ALUresult = '1';
+			*ALUresult = 1;
 		else
-			*ALUresult = '0';
+			*ALUresult = 0;
 	}
-	else if(ALUControl == '4')
+	else if((int)ALUControl == 011)
+	{
+		if((int)A < (int)B)
+			*ALUresult = 1;
+		else
+			*ALUresult = 0;
+	}
+	else if((int)ALUControl == 100)
 	{
 		*ALUresult = A & B;
 	}
-	else if(ALUControl == '5')
+	else if((int)ALUControl == 101)
 	{
 		*ALUresult = A | B;
 	}
-	else if(ALUControl == '6')
+	else if((int)ALUControl == 110)
 	{
 		*ALUresult = B << 16;
 	}
+    else if((int)ALUControl == 111)
+    {
+        *ALUresult = ~A;
+    }
 	
-	if(*ALUresult == 0)
-		*Zero = '1';
+	if(ALUresult == 0)
+		*Zero = 1;
 	else
-		*Zero = '0';
+		*Zero = 0;
 }
 
 /* instruction fetch */
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-	// if the address is in the proper format
-	if(PC % 4 == 0)
+    if(PC % 4 == 0)
+    {
 		// Get the instruction 
 		*instruction = Mem[PC >> 2];
 		return 0;
+    }
 	else
 		return 1;
 	
@@ -67,75 +72,80 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
-	*op     = (instruction & 0xfc000000) >> 26; // 11111100 00000000 00000000 00000000  
-	*r1     = (instruction & 0x03e00000) >> 21; // 00000011 11100000 00000000 00000000
-	*r2     = (instruction & 0x001f0000) >> 16; // 00000000 00011111 00000000 00000000
-	*r3     = (instruction & 0x0000f800) >> 11; // 00000000 00000000 11111000 00000000
-	*funct  =  instruction & 0x0000003f; 		// 00000000 00000000 00000000 00111111
-	*offset =  instruction & 0x0000ffff; 		// 00000000 00000000 11111111 11111111
-	*jsec   =  instruction & 0x03ffffff; 		// 00000011 11111111 11111111 11111111
+	*op     = (instruction & 0xfc000000) >> 26;
+	*r1     = (instruction & 0x03e00000) >> 21;
+	*r2     = (instruction & 0x001f0000) >> 16;
+	*r3     = (instruction & 0x0000f800) >> 11;
+	*funct  =  instruction & 0x0000003f;
+	*offset =  instruction & 0x0000ffff;
+	*jsec   =  instruction & 0x03ffffff;
 }
 
 /* instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-	// struct_controls->(RegDst, Jump, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite)
+	// struct_controls->(RegDst, Jump, Branch, MemRead, MemtoReg, ALUop, MemWrite, ALUSrc, RegWrite)
 	
 	// R Type
-	if(op = 0x0)
+	if(op = 0)
 	{
 		*controls = (struct_controls){1, 0, 0, 0, 0, 7, 0, 0, 1};
 		return 0;
 	}
 	// Add Immediate
-	else if(op == 0x8)
+	else if(op == 8)
 	{
 		*controls = (struct_controls){0, 0, 0, 0, 0, 0, 0, 1, 1};
 		return 0;
 	}
 	// Load Word
-	else if(op == 0x23)
+	else if(op == 35)
 	{
 		*controls = (struct_controls){0, 0, 0, 1, 1, 0, 0, 1, 1};
 		return 0;
 	}
 	// Store Word
-	else if(op == 0x2b)
+	else if(op == 43)
 	{
-		*controls = (struct_controls){0, 0, 0, 0, 0, 0, 1, 1, 0};
+		*controls = (struct_controls){2, 0, 0, 0, 2, 0, 1, 1, 0};
 		return 0;
 	}
 	// Load Upper Immediate
-	else if(op == 0xf)
+	else if(op == 15)
 	{
 		*controls = (struct_controls){0, 0, 0, 0, 0, 6, 0, 1, 1};
 		return 0;
 	}
 	// Branch on equal
-	else if(op == 0x4)
+	else if(op == 4)
 	{
-		*controls = (struct_controls){2, 0, 1, 0, 2, 1, 0, 2, 0};
+		*controls = (struct_controls){2, 0, 1, 0, 2, 1, 0, 0, 0};
 		return 0;
 	}
 	// Set less than immediate
-	else if(op == 0xa)
+	else if(op == 10)
 	{
 		*controls = (struct_controls){0, 0, 0, 0, 0, 2, 0, 1, 1};
 		return 0;
 	}
 	// Set less than immediate unsigned
-	else if(op == 0xb)
+	else if(op == 11)
 	{
 		*controls = (struct_controls){0, 0, 0, 0, 0, 3, 0, 1, 1};
 		return 0;
 	}
 	// Jump
-	else if(op == 0x2)
+	else if(op == 2)
 	{
-		*controls = (struct_controls){2, 1, 2, 0, 2, 0, 0, 2, 0};
+		*controls = (struct_controls){0, 1, 0, 0, 0, 0, 0, 0, 0};
 		return 0;
 	}
+    else if(op == 15)
+    {
+        *controls = (struct_controls){0, 0, 0, 0, 0, 6, 0, 1, 1};
+        return 0;
+    }
 	else
 	{
 		return 1;
@@ -157,11 +167,9 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-	// Gets the LSB
-	unsigned LSB = offset >> 15;
+	unsigned negCheckBit = offset >> 15;
 	
-	// If LSB is 1 the offset is negative.
-	if(LSB == 1)
+	if(negCheckBit == 1)
 		// Extend offset such that the value is negative
 		*extended_value = offset | 0xffff0000;
 	else
@@ -191,7 +199,7 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 		else if(funct == 37)
 			ALUOp = 5;
 		else if(funct == 6)
-			ALUOp = 6
+            ALUOp = 6;
 		else if(funct == 39)
 			ALUOp = 7;
 		else
@@ -211,19 +219,21 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 /* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
-	if((ALUresult % 4) != 0 && (MemWrite == '1' || MemRead == '1'))
-		return 1;
-	if(ALUresult > 65536 && (MemWrite == '1' ||MemRead == '1'))
-		return 1;
-	
-	ALUresult = ALUresult >> 2;
-	
-	if(MemWrite == '1')
-		Mem[ALUresult] = data1;
-	if(MemRead == '1')
-		*memdata = Mem[ALUresult];
-	
-	return 0;		
+    if (ALUresult % 4 == 0)
+    {
+        if (MemRead == 1)
+        {
+            *memdata = Mem[ALUresult >> 2];
+        }
+        if (MemWrite == 1)
+        {
+            Mem[ALUresult >> 2] = data2;
+        }
+        return 0;    }
+    else
+    {
+        return 1;
+    }
 }
 
 
@@ -231,23 +241,23 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
-	if(RegWrite == '1')
-	{
-		if(MemtoReg == '0')
-		{
-			if(RegDst == '0')
-				reg[r2] = ALUresult;
-			else if(RegDst == '1')
-				Reg[r3] = ALUresult;
-		}
-		else if(MemtoReg == '1')
-		{
-			if(RegDst == '0')
-				Reg[r2] = memdata;
-			else if(RegDst == '1')
-				reg[r3] = memdata;
-		}
-	}
+    if (RegWrite == 1)
+    {
+        if (MemtoReg == 0)
+        {
+            if (RegDst == 0)
+                Reg[r2] = ALUresult;
+            if (RegDst == 1)
+                Reg[r3] = ALUresult;
+        }
+        if (MemtoReg == 1)
+        {
+            if (RegDst == 0)
+                Reg[r2] = memdata;
+            if (RegDst == 1)
+                Reg[r3] = memdata;
+        }
+    }
 }
 
 /* PC update */
@@ -259,7 +269,7 @@ void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char 
 	if(Zero == 1 && Branch == 1)
 		*PC = *PC + (extended_value << 2);
 	
-	if(jump == 1)
+	if(Jump == 1)
 		*PC = (*PC & 0xf0000000) | (jsec << 2);
 }
 
